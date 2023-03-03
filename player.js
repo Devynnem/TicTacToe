1f91393eb7c0adeb4e9abbac1a568f0a12992a87
Devynne Marshall <devynnem@gmail.com>
2023-03-02 17:55:30 -0700

    Finally show my data model updating one function

--- player.js
+++ player.js
@@ -8,6 +8,11 @@ class Player {
         this.id = id;
         this.token = token;
         this.wins = wins;
-        this.increaseWins = 0
+        this.player1Boxes = [];
+        this.player2Boxes = [];
+        this.increaseWins = 0;
+    }
+    playerWins() {
+        this.increaseWins += 1;
     }
 }
